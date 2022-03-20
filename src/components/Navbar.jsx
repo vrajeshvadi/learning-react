526985931926a73ece11738cd9a61b7b0c1bb6f4
Vrajesh Vadi <18bce250@nirmauni.ac.in>
2022-03-20 21:42:01 +0530

    navbar completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,14 @@
+import logo from '../images/v.png';
+import './Navbar.css';
+
 export default function Navbar() {
-  return <h1>Navbar</h1>;
+  return (
+    <nav className='nav'>
+      <div className='logo'>
+        <img src={logo} alt='logo' className='logo-img' />
+        <div className='logo-txt'>Vadi</div>
+      </div>
+      <div className='project-txt'>React - Project 1</div>
+    </nav>
+  );
 }
