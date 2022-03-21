a2c978d0daba34cf3a4dedc25201942b52078523
Vrajesh Vadi <18bce250@nirmauni.ac.in>
2022-03-21 08:21:09 +0530

    project-1 completed

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,3 +1,18 @@
+import './Main.css';
+
 export default function Main() {
-  return <h1>Main</h1>;
+  return (
+    <div className='main'>
+      <div className='heading'>
+        Introduction
+      </div>
+      <ul className='facts'>
+        <li>Name: Vrajesh Vadi</li>
+        <li>Birthdate: 03 Feb 2001</li>
+        <li>Home Town: Jamnagar</li>
+        <li>Skill: Frontend Developer</li>
+        <li>Hobby: Chess, Music, Youtube</li>
+      </ul>
+    </div>
+  );
 }
