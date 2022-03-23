946e21fb62d1530c3b2293b245f97934ea067bbc
Vrajesh Vadi <18bce250@nirmauni.ac.in>
2022-03-23 09:05:25 +0530

    setup for project-2 (props)

--- src/index.js
+++ src/index.js
@@ -1,18 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Main from './components/Main';
-import Navbar from './components/Navbar';
+import App from './App.js';
 import './index.css';
 
 ReactDOM.render(
-  <div className='container'>
-    <Navbar />
-    <Main />
-  </div>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
